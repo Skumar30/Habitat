07be6f01544d89faa5f1bed26f78f592efb03a55
Andy Dong <and060@ucsd.edu>
2020-04-23 17:37:31 -0700

    Daily Tab
    Created Daily Tab for the Daily Tasks Tab

---
+++ habitat_app/src/DailyComponent/DailyTab.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class DailyTab extends Component {
+
+    render(){
+        return(
+
+    );
+
+
+}
+
+export default DailyTab;
---
+++ habitat_app/src/DailyTask.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import DailyTab from './DailyComponent/DailyTab.js';
+
+function DailyTask(){
+    return (
+        <div>
+            <DailyTab />
+        </div>
+    );
+}
+
+export default DailyTask;
