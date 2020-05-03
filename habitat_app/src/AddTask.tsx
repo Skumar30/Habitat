2b454ead3200b3e88516e9a7f307a70deed7c4e2
Darin Nguyen <danguyen01@gmail.com>
2020-05-03 06:03:37 -0700

    basic

--- habitat_app/src/AddTask.tsx
+++ habitat_app/src/AddTask.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   StyleSheet,
   View,
@@ -14,13 +14,20 @@ const addHandler = () => {
   //TODO
 }
 
-const AddTask = () => {
+export default function AddTask(){
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+
   return (
     <View style={styles.container}>
-      <Text>Title</Text>
-      <TextInput style={styles.input}/>
-      <View style={styles.buttonContainer}>
-        <Button title='Add Task' onPress={addHandler}/>
+
+      <TextInput
+        placeholder="Task Title"
+        onChangeText={(val)=>setTitle(val)}
+        style={styles.titleInput}/>
+
+      <View style={styles.button}>
+        <Button title="Add Task" style={styles.button} onPress={addHandler}/>
       </View>
     </View>
   );
@@ -28,22 +35,43 @@ const AddTask = () => {
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 50,
     flex: 2,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'flex-start'
   },
-  buttonContainer: {
-    marginTop: 100
+  titleInput: {
+    borderWidth: 3,
+    borderColor: "#000000",
+    padding: 9,
+    margin: 10,
+    backgroundColor: 'white',
+    borderRadius: 9,
+    fontSize: 16,
+    width: 300
   },
-  input: {
-    marginTop: 10,
-    borderWidth: 1,
-    borderColor: '#000',
-    width: 200,
-    padding: 8
+  descriptionInput: {
+    borderWidth: 3,
+    borderColor: "#000000",
+    padding: 9,
+    margin: 10,
+    backgroundColor: 'white',
+    borderRadius: 9,
+    fontSize: 16,
+    width: 300,
+    height: 160
+  },
+  inputTitle: {
+    marginTop: 10
+  },
+  button: {
+    borderRadius: 8,
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+    backgroundColor: '#000',
+    marginTop: 5,
+    height: 40,
+    zIndex: 1
   }
 });
-
-
-export default AddTask;
