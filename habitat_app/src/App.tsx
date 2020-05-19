5cbd2ff9b7f8c113bf436bc923648b3b7cbce4ae
Rajat Komawar <rkomawar@gmail.com>
2020-05-18 22:11:40 -0700

    trying to merge to login-and-signup branch

--- habitat_app/src/App.tsx
+++ habitat_app/src/App.tsx
@@ -10,46 +10,42 @@
 
 import React from 'react';
 import {
-    SafeAreaView,
-    StyleSheet,
-    ScrollView,
-    View,
-    Text,
-    StatusBar,
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
 } from 'react-native';
-import RegTask from './RegTask'
+import RegTask from './RegTask';
 
 import {
-    Header,
-    LearnMoreLinks,
-    Colors,
-    DebugInstructions,
-    ReloadInstructions,
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-import Home from './Home'
-import Login from './Login'
-import FriendList from './FriendList'
-import MyApp from './MyApp'
-import DailyScreen from './DailyScreen'
-import AddTask from './AddTask'
-import CreateContract from './CreateContract'
-import CustomizeScreen from './CustomizeScreen'
-import PendingCard from './PendingCard'
+import Home from './Home';
+import Login from './Login';
+import FriendList from './FriendList';
+import MyApp from './MyApp';
+import DailyScreen from './DailyScreen';
+import AddTask from './AddTask';
+import CreateContract from './CreateContract';
+import CustomizeScreen from './CustomizeScreen';
+import PendingCard from './PendingCard';
 import WellnessContractHome from './WellnessContractHome';
-import TaskCard from './TaskCard'
+import TaskCard from './TaskCard';
 import SignUp from './SignUp';
 
-declare const global: { HermesInternal: null | {} };
+declare const global: {HermesInternal: null | {}};
 
 const App = () => {
-    return (
-      <MyApp></MyApp>
-    );
+  return <MyApp></MyApp>;
 };
 
-const styles = StyleSheet.create({
-   
-});
+const styles = StyleSheet.create({});
 
 export default App;
