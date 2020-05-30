9866043f685f37629b210f95808d0127e15da4ab
Alexander Quan <aquan1204@gmail.com>
2020-05-30 15:46:09 -0700

    safety commit

--- habitat_app/habitat_server/app.js
+++ habitat_app/habitat_server/app.js
@@ -11,7 +11,7 @@ var passport = require('passport');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var MongoStore = require('connect-mongo')(session);
-var indexRouter = require('./routes/index');
+//var indexRouter = require('./routes/index');
 var addRouter = require('./routes/AddTask')
 var createRouter = require('./routes/CreateContract')
 var customizeRouter = require('./routes/Customize')
@@ -25,7 +25,7 @@ var wellnessRouter = require('./routes/WellnessContractHome')
 var usersRouter = require('./routes/users');
 var MongoStore = require('connect-mongo')(session);
 var app = express();
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true});
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 mongoose.connection.on('connected', () => {
   console.log('connected');
 });
@@ -34,34 +34,34 @@ require('./config/passport');
 app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(flash());
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-    store: new MongoStore({mongooseConnection: mongoose.connection}),
-    cookie: {maxAge: 180 * 60 * 1000}
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    cookie: { maxAge: 180 * 60 * 1000 }
   })
 );
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/addTask', addRouter);
-app.use('/createContract', createRouter)
+//app.use('/', indexRouter);
+// app.use('/addTask', addRouter);
+// app.use('/createContract', createRouter)
 app.use('/customize', customizeRouter)
-app.use('/dailyTask', dailyRouter)
-app.use('/friends', friendsRouter)
-app.use('/home', homeRouter)
-app.use('/regTask', regRouter)
-app.use('/settings', settingRouter)
-app.use('/wellnessContract', wellnessRouter)
+// app.use('/dailyTask', dailyRouter)
+// app.use('/friends', friendsRouter)
+// app.use('/home', homeRouter)
+// app.use('/regTask', regRouter)
+// app.use('/settings', settingRouter)
+// app.use('/wellnessContract', wellnessRouter)
 
 
 app.use('/users', usersRouter);
